eb94b9866b67d9594d92a1f3aa4d10bfc8c4b3a5
justinthestickman <jchuang@NN123-HA-04.HS.sd203.org>
2015-09-29 15:04:12 -0500

    Annuity

--- Chapter 4 Practice/Annuity.java
+++ Chapter 4 Practice/Annuity.java
@@ -3,7 +3,12 @@ public class Annuity
     public static void main(String[] args)
     {
         double pmt = 10000;
-        double i = 0.08;
         double n = 20;
+        final double I = 0.08;
+        final double BASE = 1+I;
+        final double EXP = n-1;
+        final double X = Math.pow(BASE,EXP);
+        double pvann = pmt*((((X-1)/I)/X)+1);
+        System.out.println(pvann);
     }
 }
